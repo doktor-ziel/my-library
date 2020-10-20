a2a7d3686a19820ac4712d6d3bfc99197b10337e
Doktor Ziel <doktor@zielony-backlog.pl>
2020-10-20 21:53:41 +0200

    First attepmt - only test fails

---
+++ src/main/java/pl/backlog/green/GeniusLibrary.java
@@ -0,0 +1,7 @@
+package pl.backlog.green;
+
+public class GeniusLibrary {
+	public static double calculateAreaOfCircle(double radius) {
+		return radius * radius * Math.PI;
+	}
+}
---
+++ src/test/java/pl/backlog/green/GeniusLibraryTest.java
@@ -0,0 +1,18 @@
+package pl.backlog.green;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class GeniusLibraryTest {
+    @Test
+    public void methodShouldReturnCorrectValue() {
+        // given
+		
+		// when
+		double area = GeniusLibrary.calculateAreaOfCircle(4.0);
+		
+		// then
+		assertEquals(50.2655, area);
+    }
+}
